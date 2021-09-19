1b03bbac31a1c2b062a86526ec2ef66f1fd79a92
Lucas Henrique Paes de Carvalho <lukas.paes18@gmail.com>
2021-09-19 02:21:54 -0300

    chore: mongoose

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const app = express();
+const bodyPaser = require("body-parser");
 
 const mongan = require("morgan");
+const routes = require("./src/routes/main.routes");
 
 app.use(mongan("dev"));
+app.use(bodyPaser.json());
 
-app.get("/", (req, res) => {
-  res.json({ message: "rota funcionando" });
-});
+app.use("/", routes);
 
 app.listen(3000, () => {
   console.log("Online");
